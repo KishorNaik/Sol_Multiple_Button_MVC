a4a72548da1996adb6cd057c5199982dc4a941c5
Kishor Naik <kishor.naik011.net@gmail.com>
2019-05-25 17:20:43 +0530

    Code Commit

---
+++ Sol_Multiple_Button/Sol_Multiple_Button/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Multiple_Button.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OnSubmit1()
+        {
+            ViewBag.Submit1Status = "Submit 1 Called";
+            return View("Index");
+        }
+
+        public IActionResult OnSubmit2()
+        {
+            ViewBag.Submit2Status = "Submit 2 Called";
+            return View("Index");
+        }
+    }
+}
